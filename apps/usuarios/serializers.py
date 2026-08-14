381ff1a25d85f7f4dafe184f0c9560c46e435d38
wil <wil.fabri777@gmail.com>
2026-08-13 23:18:50 -0400

    07-CRUD - Serializer de usuarios

--- apps/usuarios/serializers.py
+++ apps/usuarios/serializers.py
@@ -2,14 +2,49 @@ from rest_framework import serializers
 from .models import Usuario
 
 class UsuarioSerializer(serializers.ModelSerializer):
+    # Campo password solo para escritura
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = Usuario
         fields = [
             'id',
             'username',
             'email',
+            'password',
             'first_name',
             'last_name',
-            'is_staff',
             'is_active',
+            'last_login',
+            'date_joined',
         ]
+
+        # Campos solo lectura
+        read_only_fields = [
+            'id',
+            'last_login',
+            'date_joined',
+        ]
+
+    # Crear usuario
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+
+        usuario = Usuario(**validated_data)
+        usuario.set_password(password)
+        usuario.save()
+        
+        return usuario
+
+    # Actualizar usuario
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        
+        if password:
+            instance.set_password(password)
+        
+        instance.save()
+        return instance
