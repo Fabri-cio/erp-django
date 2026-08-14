2682b751b68ba43331fd2bb872ded6e882ddaba4
wil <wil.fabri777@gmail.com>
2026-08-13 22:12:23 -0400

    06-Roles y permisos

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,11 +1,11 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
 
 from .models import Usuario
 from .serializers import UsuarioSerializer
+from .permissions import UsuarioPermission
 
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [UsuarioPermission]
